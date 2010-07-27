7682ff5e499907d124ab10854c9feb8b2764dcae
Your Name <anders.feder.83@gmail.com>
2010-07-27 12:40:14 +0800

    Made function return interleaved vector.

--- gwn.m
+++ gwn.m
@@ -2,10 +2,10 @@
 % by Anders Feder
 %
 % This function generates GWN, with a seed file, using the Box-Muller
-% transform. Due to the way the transform works, two GWN vectors are returned,
-% Z0 and Z1.
+% transform. The two GWN vectors produced by the transform are interleaved to
+% create a single vector of size blocksize*2.
 
-function Z0, Z1 = gwn ( blocksize, filename )
+function Z = gwn ( blocksize, filename )
   persistent fd;
   if (exist("filename"))
     if (isscalar(fd))
@@ -21,7 +21,7 @@ function Z0, Z1 = gwn ( blocksize, filename )
   for n = 1:blocksize
     u1 = (b1(n)+1)/256;
     u2 = (b2(n)+1)/256;
-    Z0(n) = sqrt(-2*log(u1))*cos(2*pi*u2);
-    Z1(n) = sqrt(-2*log(u1))*sin(2*pi*u2);
+    Z(n*2+1) = sqrt(-2*log(u1))*cos(2*pi*u2);
+    Z(n*2+2) = sqrt(-2*log(u1))*sin(2*pi*u2);
   endfor
 endfunction
